206dd084a64bcdc469ee3c7c9037125ccbdd2a1d
Brumagint/Desktop <brumagint@gmail.com>
2020-09-07 19:33:33 -0400

    Cleaned up Toxic Emm.

--- src/main/java/Spansion/Cards/ToxicEmissions.java
+++ src/main/java/Spansion/Cards/ToxicEmissions.java
@@ -3,10 +3,13 @@ package Spansion.Cards;
 import Spansion.Orbs.ToxicOrb;
 import Spansion.Spansion;
 import basemod.abstracts.CustomCard;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.actions.defect.ChannelAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
@@ -22,51 +25,43 @@ import java.util.ArrayList;
 import static Spansion.Spansion.makeCardPath;
 
 public class ToxicEmissions extends CustomCard {
-    // "Toxic Emissions" - Skill - 1 - Apply 1 weak and 1 poison to all enemies.
-
-    public static final String ID = Spansion.makeID(ToxicEmissions.class.getSimpleName());
+    // Toxic Emissions - Skill - 0 - Apply 1 weak and vulnerable to an enemy for each different type of orb.
+    public static final String ID = Spansion.makeID(OldOneTwo.class.getSimpleName());
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
 
-    public static final String IMG = makeCardPath("ToxicEmissions.png");
+    public static final String IMG = makeCardPath("Skill.png");
+
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
 
     private static final CardRarity RARITY = CardRarity.COMMON;
-    private static final CardTarget TARGET = CardTarget.NONE;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
     private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = CardColor.BLUE;
 
     private static final int COST = 0;
-    private static final int WEAK = 1;
-    private static final int UPGRADE_WEAK = 1;
+    private static final int WEAK_VULN_PER_UNIQUE_ORB = 1;
 
     public ToxicEmissions() {
         super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
 
-        baseMagicNumber = magicNumber = WEAK;
+        magicNumber = baseMagicNumber = WEAK_VULN_PER_UNIQUE_ORB;
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-
-        ArrayList<String> orbList = new ArrayList<>();
-        for (AbstractOrb o : AbstractDungeon.player.orbs) {
-            if (o.ID != null && !o.ID.equals("Empty") && !orbList.contains(o.ID))
-            { orbList.add(o.ID);
+        ArrayList<String> orbList = new ArrayList<String>();
+        for(AbstractOrb o: AbstractDungeon.player.orbs){
+            if(o.ID != null
+                    && !o.ID.equals("Empty")
+                    && !orbList.contains(o.ID)){
+                orbList.add(o.ID);
             }
         }
-        int x = orbList.size() * magicNumber;
-
-        ArrayList<AbstractMonster> mons = AbstractDungeon.getCurrRoom().monsters.monsters;
-        for(int i = 0; i < mons.size(); i++) {
-            m = mons.get(i);
-            if(m.currentHealth > 0 && !(m.isPlayer)) {
-                AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(m, p, new WeakPower(m, x, false), magicNumber));
-                AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(m, p, new VulnerablePower(m, x, false), magicNumber));
-            }
-        }
-
+        int amount = orbList.size() * magicNumber;
+        AbstractDungeon.actionManager.addToBottom( new ApplyPowerAction(m, p, new VulnerablePower(m, amount, false)));
+        AbstractDungeon.actionManager.addToBottom( new ApplyPowerAction(m, p, new WeakPower(m, amount, false)));
     }
 
     // Upgraded stats.
@@ -74,7 +69,7 @@ public class ToxicEmissions extends CustomCard {
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            upgradeMagicNumber(UPGRADE_WEAK);
+            upgradeMagicNumber(WEAK_VULN_PER_UNIQUE_ORB);
             initializeDescription();
         }
     }
