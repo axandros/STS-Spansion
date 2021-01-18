4388b8b5b5bcf1475cf9e6c064360e6fe8ccf0f1
Brumagint <brumagint@outlook.com>
2021-01-18 16:02:29 -0500

    Bugfix on Gen Shop crash.

--- src/main/java/Spansion/Events/GeneralShopEvent.java
+++ src/main/java/Spansion/Events/GeneralShopEvent.java
@@ -105,13 +105,16 @@ public class GeneralShopEvent extends AbstractImageEvent {
         } else if( giveCard
                 &&!AbstractDungeon.isScreenUp
                 && !AbstractDungeon.gridSelectScreen.selectedCards.isEmpty()) {
-            AbstractCard c = AbstractDungeon.gridSelectScreen.selectedCards.remove(0);
+            AbstractCard c = AbstractDungeon.gridSelectScreen.selectedCards.get(0);
+            AbstractDungeon.gridSelectScreen.selectedCards.clear();
             AbstractDungeon.topLevelEffects.add(new PurgeCardEffect(c, (Settings.WIDTH / 2), (Settings.HEIGHT / 2)));
+            //AbstractDungeon.topLevelEffects.add(new PurgeCardEffect(AbstractDungeon.gridSelectScreen.selectedCards.get(0), (Settings.WIDTH / 2), (Settings.HEIGHT / 2)));
             AbstractDungeon.player.masterDeck.removeCard(c);
             AbstractDungeon.player.obtainPotion(AbstractDungeon.returnRandomPotion(AbstractPotion.PotionRarity.COMMON,false));
             this.imageEventText.updateBodyText(DESCRIPTIONS[2]);
             this.imageEventText.updateDialogOption(0, OPTIONS[5]);
             this.imageEventText.clearRemainingOptions();
+            Spansion.logger.info("Screen Set to Complete.");
             screen = CUR_SCREEN.COMPLETE;
         }
 
@@ -139,9 +142,8 @@ public class GeneralShopEvent extends AbstractImageEvent {
                         this.pickCard = true;
                         break;
                     case 1: // "Give a card, get 1 random potion",
-                        // Status: Crashes the game.
+                        // Status: Crashes when potions full?
                         giveCard = true;
-                        //AbstractPotion
                         if(CardGroup.getGroupWithoutBottledCards(AbstractDungeon.player.masterDeck.getPurgeableCards()).size() > 0){
                             AbstractDungeon.gridSelectScreen.open(
                             CardGroup.getGroupWithoutBottledCards(
@@ -181,10 +183,11 @@ public class GeneralShopEvent extends AbstractImageEvent {
                 }
                 break;
             case COMPLETE:
-                switch(i){
-                    default:
+                //switch(i){
+                //    case 0:
+                //    default:
                         openMap();
-                }
+                //}
 
 
         }
