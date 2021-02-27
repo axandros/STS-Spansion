04d1f5f97551abba529cba1f6da75c5adcf2c80b
Brumagint <brumagint@outlook.com>
2021-02-27 15:59:03 -0500

    Added code to ComplexCardEvent.  Ocasionally crashes the game.

--- src/main/java/Spansion/Events/ComplexCardEvent.java
+++ src/main/java/Spansion/Events/ComplexCardEvent.java
@@ -1,9 +1,19 @@
 package Spansion.Events;
 
 import Spansion.Spansion;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardGroup;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.events.AbstractImageEvent;
 import com.megacrit.cardcrawl.localization.EventStrings;
+import com.megacrit.cardcrawl.vfx.cardManip.ShowCardAndObtainEffect;
+import com.megacrit.cardcrawl.vfx.cardManip.ShowCardBrieflyEffect;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 
 import static Spansion.Spansion.makeEventPath;
 
@@ -22,6 +32,7 @@ public class ComplexCardEvent extends AbstractImageEvent {
     private CUR_SCREEN screen = CUR_SCREEN.INTRO;
 
     private boolean chooseUpgrade = false;
+    private int upgrades = 2;
     private boolean chooseTransform = false;
     private boolean chooseDuplicate = false;
 
@@ -30,7 +41,7 @@ public class ComplexCardEvent extends AbstractImageEvent {
 
         imageEventText.setDialogOption(OPTIONS[0]);
         imageEventText.setDialogOption(OPTIONS[1]);
-        imageEventText.setDialogOption(OPTIONS[2]);
+        imageEventText.setDialogOption(OPTIONS[2] + upgrades + OPTIONS[3]);
     }
 
     @Override
@@ -38,17 +49,36 @@ public class ComplexCardEvent extends AbstractImageEvent {
         switch(screen){
             case INTRO:
                 switch (i){
-                    case 0: // Add a card to your deck
+                    case 0: // Transform a card
                         imageEventText.updateBodyText(DESCRIPTIONS[1]);
+                        AbstractDungeon.gridSelectScreen.open(
+                                CardGroup.getGroupWithoutBottledCards(AbstractDungeon.player.masterDeck
+                                .getPurgeableCards()), 1, OPTIONS[2], false, true, false, false);
+                        chooseTransform = true;
                         break;
-                    case 1: // Remove a card
-                        imageEventText.updateBodyText(DESCRIPTIONS[2]);
-                        break;
-                    case 2: // Add a skill
+                    case 1: // Duplicate
                         imageEventText.updateBodyText(DESCRIPTIONS[1]);
+                        chooseDuplicate = true;
+                        AbstractDungeon.gridSelectScreen.open(AbstractDungeon.player.masterDeck, 1, OPTIONS[2], false, false, false, false);
                         break;
-                    case 3: // Remove a skill
-                        imageEventText.updateBodyText(DESCRIPTIONS[2]);
+                    case 2: // Upgrade N Cards
+                        //CardGroup group = AbstractDungeon.player.masterDeck
+                        ArrayList<AbstractCard> upgradableCards = new ArrayList<AbstractCard>();
+                        for (AbstractCard c:AbstractDungeon.player.masterDeck.group ) {
+                            if(c.canUpgrade())
+                            {upgradableCards.add(c);}
+                        }
+                        Collections.shuffle(upgradableCards, new Random(AbstractDungeon.miscRng.randomLong()));
+                        for(int j = 0; j < upgrades && j < upgradableCards.size(); j++) {
+                            AbstractCard card = upgradableCards.get(j);
+                            card.upgrade();
+                            AbstractDungeon.player.bottledCardUpgradeCheck(card);
+                            float offsetX = AbstractDungeon.miscRng.random(-200, 200);
+                            float offsetY = AbstractDungeon.miscRng.random(-200, 200);
+                            AbstractDungeon.effectList.add(new ShowCardBrieflyEffect(card.makeStatEquivalentCopy()
+                                    , Settings.WIDTH / 2.0F +offsetX * Settings.scale, Settings.HEIGHT / 2.0F + offsetY));
+                        }
+                        imageEventText.updateBodyText(DESCRIPTIONS[1]);
                         break;
                 }
                 imageEventText.updateDialogOption(0, OPTIONS[4]);
@@ -58,6 +88,31 @@ public class ComplexCardEvent extends AbstractImageEvent {
             case COMPLETE:
                 openMap();
                 break;
+        }
+    }
+
+    @Override
+    public void update() {
+        super.update();
+        if (!AbstractDungeon.isScreenUp &&
+                !AbstractDungeon.gridSelectScreen.selectedCards.isEmpty()) {
+            AbstractCard c = AbstractDungeon.gridSelectScreen.selectedCards.get(0);
+            if (chooseTransform) {
+                AbstractDungeon.player.masterDeck.removeCard(c);
+                AbstractDungeon.transformCard(c, false, AbstractDungeon.miscRng);
+                AbstractCard transCard = AbstractDungeon.getTransformedCard();
+                AbstractDungeon.effectList.add(new ShowCardAndObtainEffect(transCard, Settings.WIDTH / 2.0F, Settings.HEIGHT / 2.0F));
+            }
+            if(chooseDuplicate) {
+                AbstractCard dupe = c.makeStatEquivalentCopy();
+                dupe.inBottleFlame = false;
+                dupe.inBottleLightning = false;
+                dupe.inBottleTornado = false;
+                AbstractDungeon.effectList.add(new ShowCardAndObtainEffect(dupe, Settings.WIDTH / 2.0F, Settings.HEIGHT / 2.0F));
+            }
+
+
+            AbstractDungeon.gridSelectScreen.selectedCards.clear();
         }
     }
 }
